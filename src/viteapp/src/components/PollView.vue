c4a597a9c1930480df1e91bc597f1c888d0ef773
tsuyo-itai <suka_sudeilu@yahoo.co.jp>
2023-09-22 22:44:28 +0900

    全体的にvueの整理

--- src/viteapp/src/components/PollView.vue
+++ src/viteapp/src/components/PollView.vue
@@ -1,3 +1,54 @@
+<script setup>
+import { ref, inject, onMounted, computed } from 'vue';
+import CryptoJS from 'crypto-js';
+const axios = inject('axios')
+import { useRoute } from 'vue-router';
+
+// Vue Routerのルート情報を取得
+const route = useRoute();
+
+// TODO: 環境変数へ
+const secretKey = 'mysecretkey';
+
+
+// const randomToken = route.params.random_token
+const randomToken = route.params.random_token;
+console.log(randomToken)
+
+const PollChoice = ref([])
+
+// 暗号化文字列の復号化
+function decryptSafeEncryptedText(safeEncryptedText) {
+    console.log(safeEncryptedText)
+    // 1. '_' を '/' に戻す
+    const base64CipherText = safeEncryptedText.replace(/_/g, '/'); // 例: '_' を '/' に戻す
+    // 2. Base64デコード
+    const cipherText = atob(base64CipherText);
+    // 3. 暗号文を復号化
+    const bytes = CryptoJS.AES.decrypt(cipherText, secretKey);
+    const originalText = bytes.toString(CryptoJS.enc.Utf8);
+
+    return originalText;
+}
+
+function getPollChoiceOne() {
+    console.log(randomToken)
+    const pollid = decryptSafeEncryptedText(randomToken)
+    axios.get(`/api/v1/poll/${pollid}`).then((response) => {
+    console.log(response.data.title);
+    PollChoice.value = response.data;
+    });
+}
+
+onMounted(() => { getPollChoiceOne()
+    console.log(PollChoice)
+
+});
+
+</script>
+
 <template>
     <h2>投票作成が完了しました画面</h2>
+    <h3>{{ PollChoice.title }}</h3>
+    <h4>{{ PollChoice.description }}</h4>
 </template>
--- src/viteapp/src/main.js
+++ src/viteapp/src/main.js
@@ -2,30 +2,17 @@ import { createApp } from 'vue'
 import './style.css'
 import './scss/style.scss'
 import App from './App.vue'
-import MainView from './components/MainView.vue'
-import EditPollView from './components/EditPollView.vue'
-import PollView from './components/PollView.vue'
-import axios from 'axios';
-import { createRouter, createWebHistory } from 'vue-router';
 import VueAxios from 'vue-axios';
-
+import axiosInstance from "./axios/index"
+import router from "./router/index"
 
 const app = createApp(App);
-// axiosのインスタンスを作成してbaseURLを設定
-const axiosInstance = axios.create({
-    baseURL: 'http://127.0.0.1:8000', // APIサーバーの設定 (TODO:本番環境では環境変数から読み込むように)
-});
 
-// routerのインスタンスを作成してパスを設定
-const router = createRouter({
-    history: createWebHistory(),
-    routes: [
-      { path: '/', name: "root", component: MainView },
-      { path: '/editPoll', name: "editPoll", component: EditPollView },
-      { path: '/Poll', name: "Poll", component: PollView }, //TODO パスはオリジナルURLとする 一旦適当に作成
-    ],
-});
+console.log(app.config.globalProperties)
 
 app.use(router);
 app.use(VueAxios, axiosInstance);
+// 外部から触れるように
+app.provide('axios', app.config.globalProperties.axios)
+app.provide('router', app.config.globalProperties.$router)
 app.mount('#app')
