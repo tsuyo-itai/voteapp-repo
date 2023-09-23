f536d5eae12a0a088f537831acdfa9ec0c47c474
tsuyo-itai <suka_sudeilu@yahoo.co.jp>
2023-09-24 07:00:05 +0900

    競合マージ部分の修正

--- src/viteapp/src/main.js
+++ src/viteapp/src/main.js
@@ -2,12 +2,6 @@ import { createApp } from 'vue'
 import './style.css'
 import './scss/style.scss'
 import App from './App.vue'
-import MainView from './components/MainView.vue'
-import EditPollView from './components/EditPollView.vue'
-import EditPollCompleteView from './components/EditPollComplete.vue'
-import PollView from './components/PollView.vue'
-import axios from 'axios';
-import { createRouter, createWebHistory } from 'vue-router';
 import VueAxios from 'vue-axios';
 import axiosInstance from "./axios/index"
 import router from "./router/index"
@@ -15,16 +9,6 @@ import router from "./router/index"
 const app = createApp(App);
 
 console.log(app.config.globalProperties)
-// routerのインスタンスを作成してパスを設定
-const router = createRouter({
-    history: createWebHistory(),
-    routes: [
-      { path: '/', name: "root", component: MainView },
-      { path: '/editPoll', name: "editPoll", component: EditPollView },
-      { path: '/editPollComplete', name: "editPollComplete", component: EditPollCompleteView },
-      { path: '/Poll', name: "Poll", component: PollView }, //TODO パスはオリジナルURLとする 一旦適当に作成
-    ],
-});
 
 app.use(router);
 app.use(VueAxios, axiosInstance);
--- src/viteapp/src/router/index.js
+++ src/viteapp/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import MainView from '../components/MainView.vue'
 import EditPollView from '../components/EditPollView.vue'
 import PollView from '../components/PollView.vue'
+import EditPollCompleteView from '../components/EditPollComplete.vue'
 
 // routerのインスタンスを作成してパスを設定
 const router = createRouter({
@@ -11,6 +12,7 @@ const router = createRouter({
       { path: '/editPoll', name: "editPoll", component: EditPollView },
       // { path: '/Poll', name: "Poll", component: PollView }, //TODO パスはオリジナルURLとする 一旦適当に作成
       { path: "/Poll/:random_token", name: "vote", component: PollView },
+      { path: '/editPollComplete', name: "editPollComplete", component: EditPollCompleteView },
     ],
 });
 
